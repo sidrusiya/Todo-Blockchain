2c93c68d536128b7053e0b72353aa535fc6e088d
sidrusiya <51047433+sidrusiya@users.noreply.github.com>
2021-05-21 01:05:01 +0530

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Todolist = artifacts.require("./Todolist.sol");
+var TodoList = artifacts.require("./TodoList.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Todolist);
+  deployer.deploy(TodoList);
 };
